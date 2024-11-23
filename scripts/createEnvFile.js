825a449a005cbb9170d7b96a6673480f58f4d37d
shadowsoffortune <contact@shadowsoffortune.com>
2024-11-23 22:58:37 +0100

    fix github action

---
+++ scripts/createEnvFile.js
@@ -0,0 +1,9 @@
+// scripts/createEnvFile.js
+const fs = require('fs');
+
+const envContent = `
+SONIC_PRIVATE_KEY=${process.env.SONIC_PRIVATE_KEY}
+SONIC_TESTNET_URL=${process.env.SONIC_TESTNET_URL}
+`;
+
+fs.writeFileSync('.env', envContent.trim());
